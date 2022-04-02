cf34b962094cc6f571e1fc97685ada85f2a15326
Studio8100 <studio8100@yahoo.com>
2022-04-02 07:12:27 -0400

    site ready for upload, mobile views corrected

--- script.js
+++ script.js
@@ -50,3 +50,17 @@ nav3.addEventListener('click', toggleNav);
 nav4.addEventListener('click', toggleNav);
 nav5.addEventListener('click', toggleNav);
 
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('socialAnimation');
+    }
+  });
+  });
+  
+  // Tell the observer which elements to track
+  observer.observe(document.querySelector('.socialAnimationArea'));
